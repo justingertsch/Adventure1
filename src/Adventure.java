35113dfdab7c88b6477e63cfd5cec5ac8c3ffad3
Justin Gertsch <justin.gertsch@gmail.com>
2014-08-28 20:42:46 -0600

    Flushing out methods

--- src/Adventure.java
+++ src/Adventure.java
@@ -1,7 +1,47 @@
 /**
  * Created by lucian on 8/27/14.
  */
+
+import java.util.Scanner;
+
 public class Adventure
 {
+    final int MIN_ROW = 0;
+    final int MAX_ROW = 4;
+    int eastwest = 0;
+    int northsouth = 0;
+    Scanner inputscanner = new Scanner();
+    String[] inventory;
+
+    public static void main(String[] args)
+    {
+        do
+        {
+            String input = inputscanner.nextLine();
+            String[] cmds = input.split(" +");
+
+        }while();
+    }
+
+    // go method
+    public void go(String direction)
+    {
+
+    }
+
+    // inventory method
+
+    public void showInventory()
+    {
+        System.out.println("");
+    }
+
+    // print location method
+
+    public void printLocation()
+    {
+
+    }
+
 
 }
